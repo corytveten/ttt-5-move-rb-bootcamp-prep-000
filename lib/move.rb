8c375aa207acc74b4ad210478126dcff740483c1
corytveten <corytveten@gmail.com>
2018-04-08 16:01:52 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,8 +7,8 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(num)
-  new_num = num.to_i
+def input_to_index(input)
+  new_num = input.to_i
   new_num -= 1
   return new_num
 end
